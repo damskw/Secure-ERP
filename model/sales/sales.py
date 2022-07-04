589be49dcc792fba4af4a37d16548d40e07c2e8d
damskw <damian.skwierawski@gmail.com>
2022-07-04 10:48:25 +0200

    Introduce: first working function, get_customers()

--- model/sales/sales.py
+++ model/sales/sales.py
@@ -8,7 +8,18 @@ Data table structure:
     - transaction date (string): in ISO 8601 format (like 1989-03-21)
 """
 
+import sys
+sys.path.append('./')
 from model import data_manager, util
 
 DATAFILE = "model/sales/sales.csv"
 HEADERS = ["Id", "Customer", "Product", "Price", "Date"]
+
+
+def get_customers():
+    all_data = data_manager.read_table_from_file(DATAFILE, separator=";")
+    customers = []
+    customer_position = 2
+    for line in all_data:
+        customers.append(line[customer_position])
+    return customers
